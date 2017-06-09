cad7a3b5561fab0ab17e51f0a0412fb1e1240781
mikesiers <msiers@csu.edu.au>
2017-06-09 22:43:12 +1000

    Fixed a simple mistake where the parenthesis was in the wrong spot.

--- tests/calculation-tests.py
+++ tests/calculation-tests.py
@@ -5,7 +5,7 @@ import unittest
 
 class test_calculations(unittest.TestCase):
     def test_calculates_entropy(self):
-        rounded_entropy = round(calculate_entropy([2, 3], 5))
+        rounded_entropy = round(calculate_entropy([2, 3]), 5)
         self.assertEqual(rounded_entropy, 0.97095)
 
 if __name__ == '__main__':
