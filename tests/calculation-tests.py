ef16c23997b0a7027b66e85cf4fdce609afe2dae
mikesiers <msiers@csu.edu.au>
2017-06-09 13:10:23 +1000

    Wrote test for the calculate_entropy function which has not been implemented. The test assumes that the function will take two numbers as input, which is the support for and support not for a class value of interest.

---
+++ tests/calculation-tests.py
@@ -0,0 +1,10 @@
+from entro-py import calculate_entropy
+import unittest
+
+class test_calculations(unittest.TestCase):
+    def calculates_entropy(self):
+        rounded_entropy = round(calculate_entropy(2, 3), 5)
+        self.assertEqual(rounded_entropy, 0.97095)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
