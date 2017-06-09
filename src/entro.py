40c67718161f9457dc8507e8664ef90443427aa2
mikesiers <msiers@csu.edu.au>
2017-06-09 17:49:22 +1000

    Added descriptive docstrings. The purpose for doing so is for use in pydoc, which I'm still working out how to use.

--- src/entro.py
+++ src/entro.py
@@ -1,6 +1,25 @@
+"""entro.py : Information Entropy Calculations
+
+This module can be used to calculate various information entropy based
+measurements. It is mainly intended for use in machine learning algorithms.
+
+"""
 import math
 
 def calculate_entropy(support_true, support_false):
+    """This function can be used to calculate information entropy.
+
+    Args:
+        support_true (int): The number of data points considered positive.
+        support_false (int): The number of data points considered negative.
+
+    Returns:
+        (num): The information entropy given the two support counts.
+
+    Raises:
+        ValueError: If one or more of the support values are negative.
+
+    """
     if (support_true < 0 or support_false < 0):
         raise ValueError('One or more input support values was negative.')
     support_total = support_true + support_false
