c37c752fd5c7327b81d0f58e8530f73caeaa57f8
mikesiers <msiers@csu.edu.au>
2017-06-09 22:20:58 +1000

    Re-wrote calculate_entropy so that it can take more than just two class support counts. This is a necessary part of calculating entropy since not all problems will have just two class values. I used list comprehension but this may have not been the fastest way to do it, however, it is clean.

--- src/entro.py
+++ src/entro.py
@@ -6,25 +6,26 @@ measurements. It is mainly intended for use in machine learning algorithms.
 """
 import math
 
-def calculate_entropy(support_true, support_false):
+def calculate_entropy(supports):
     """This function can be used to calculate information entropy.
 
     Args:
-        support_true (int): The number of data points considered positive.
-        support_false (int): The number of data points considered negative.
+        supports (array<int>): The number of data points for each class value.
 
     Returns:
-        (num): The information entropy given the two support counts.
+        (num): The information entropy given the support counts.
 
     Raises:
-        ValueError: If one or more of the support values are negative.
+        ValueError: If one or more of the support values are 0 or less.
 
     """
-    if (support_true < 0 or support_false < 0):
-        raise ValueError('One or more input support values was negative.')
-    support_total = support_true + support_false
-    prior_true = support_true/support_total
-    prior_false = support_false/support_total
+    if any(i <= 0 for in in supports):
+        raise ValueError('One or more input support values was 0 or less.')
+    support_total = sum(supports)
 
-    return  -prior_true * math.log(prior_true, 2) + \
-            -prior_false * math.log(prior_false, 2)
+    entropy = 0
+    priors = [x / support_total for x in supports]
+    for prior in priors:
+        entropy += -prior * math.log(prior, 2)
+
+    return entropy
