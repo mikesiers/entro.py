8c84d04cfcf41a6f2f5586dfb8d145249118208c
mikesiers <msiers@csu.edu.au>
2017-06-14 11:22:36 +1000

    Closes #2: Added two more tests which test if the parent support values passed to the info_gain() function raise ValueError exceptions when they are 1) equal to zero and 2) less than zero.

--- tests/calculation-tests.py
+++ tests/calculation-tests.py
@@ -9,10 +9,10 @@ class test_calculations(unittest.TestCase):
         self.assertEqual(round(entropy([2, 3]), 5), 0.97095)
         # Test a case where there is only one class. 
         self.assertEqual(entropy([4]), 0)
-        # Test that a ValueError exception is raised when a support is = 0.
+        # Test that ValueError exception is raised when a support = 0.
         with self.assertRaises(ValueError):
             entropy([1, 3, 0])
-        # Test that a ValueError exception is raised when a support is < 0.
+        # Test that ValueError exception is raised when a support < 0.
         with self.assertRaises(ValueError):
             entropy([1, 3, -5])
 
@@ -20,12 +20,17 @@ class test_calculations(unittest.TestCase):
         # Test a simple split with two resulting children.
         gain = info_gain([[3, 1], [1, 5]], [4, 6])
         self.assertEqual(round(gain, 5), 0.25643)
-        # Test that a ValueError exception is raised when a support is = 0.
+        # Test that ValueError exception is raised when a split support = 0.
         with self.assertRaises(ValueError):
             info_gain([[1, 3, 0], [2, 5, 1]], [3, 2, 9])
-        # Test that a ValueError exception is raised when a support is < 0.
+        # Test that ValueError exception is raised when a split support < 0.
         with self.assertRaises(ValueError):
             info_gain([[1, 3, -5], [2, 5, 1]], [3, 2, 9])
-
+        # Test that ValueError exception is raised when a parent support =  0.
+        with self.assertRaises(ValueError):
+            info_gain([[1, 3, 8], [2, 5, 1]], [3, 0, 9])
+        # Test that ValueError exception is raised when a parent support < 0.
+        with self.assertRaises(ValueError):
+            info_gain([[1, 3, 5], [2, 5, 1]], [3, -6, 9])
 if __name__ == '__main__':
     unittest.main(exit=False)
