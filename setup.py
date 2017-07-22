199a827039fd2ad7aaf6a14f8629f1c9bef3a21e
mikesiers <msiers@csu.edu.au>
2017-07-22 17:14:21 +1000

    docs: Added setup files for PyPi submission.
    To make entro.py available using Collecting entro, these setup files are required.
    
    See GitHub Issue #6.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'entro',
+  packages = ['entro'],
+  version = '1.0',
+  description = 'Information entropy measurements library',
+  author = 'Michael J. Siers',
+  author_email = 'mikesiers@live.com',
+  url = 'https://github.com/mikesiers/entroy.py/',
+  download_url = 'https://github.com/mikesiers/entro.py/archive/1.0.tar.gz',
+  keywords = ['information-entropy', 'machine-learning', 'data-science'],
+  classifiers = []
+)
