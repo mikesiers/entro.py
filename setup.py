121e10e92c1001e1e4d7c11933642834110e9d3b
mikesiers <msiers@csu.edu.au>
2017-07-22 17:51:21 +1000

    bug: Fixed typo in repo url.
    Accidentally wrote 'entroy.py' instead of entro.py.
    
    See GitHub Issue #6.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
-  name = 'entro',
-  packages = ['entro'],
+  name = 'Entro.py',
+  packages = ['src'],
   version = '1.0',
   description = 'Information entropy measurements library',
   author = 'Michael J. Siers',
   author_email = 'mikesiers@live.com',
-  url = 'https://github.com/mikesiers/entroy.py/',
+  url = 'https://github.com/mikesiers/entro.py/',
   download_url = 'https://github.com/mikesiers/entro.py/archive/1.0.tar.gz',
   keywords = ['information-entropy', 'machine-learning', 'data-science'],
   classifiers = []
